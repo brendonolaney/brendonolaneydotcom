a919f3da653fc857ec105a6f45828f208b321e8d
Brendon O'Laney <brendon.olaney@protonmail.com>
2021-03-06 14:37:27 -0800

    fix build script again

--- build.sh
+++ build.sh
@@ -2,29 +2,44 @@
 
 set -e
 
-for file in $(find src -type f)
-do
+render_file() {
+    input_dir="$1"
+    input_file="$2"
 
-    input_dir="$(dirname "${file}")"
-    input_file="$(basename "${file}")"
+    output_dir="${input_dir}"
+    mkdir -p "www/${output_dir}"
 
-    printf "rendering %s/%s as " "${input_dir}" "${input_file}"
-
-    output_dir="www/${input_dir#src/}"
-    mkdir -p "${output_dir}"
-
+    printf "rendering src%s/%s as " "${input_dir}" "${input_file}"
     case "${input_file}" in
         *.org)
             output_file="${input_file%.org}.html"
-            pandoc --standalone --from org --to html5 --output="${output_dir}/${output_file}" "${file}"
-            printf "%s" "${output_dir}/${output_file}"
+            pandoc --standalone --from org --to html5 \
+                --output="www/${output_dir}/${output_file}" \
+                "src/${input_dir}/${input_file}"
             ;;
         *)
             output_file="${input_file}"
-            cp "${input_dir}/${input_file}" "${output_dir}/${output_file}"
-            printf "%s" "${output_dir}/${output_file}"
+            cp "src/${input_dir}/${input_file}" "www/${output_dir}/${output_file}"
             ;;
     esac
+    printf "%s\n" "www${output_dir}/${output_file}"
+}
 
-    printf "\n"
-done
+main () {
+    for file in src/*.*
+    do
+        input_file="$(basename "${file}")"
+        render_file "" "${input_file}"
+    done
+
+    for file in src/**/*.*
+    do
+
+        input_dir="$(dirname "${file}")"
+        input_file="$(basename "${file}")"
+
+        render_file "${input_dir#src}" "${input_file}"
+    done
+}
+
+main
