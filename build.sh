5f26f5db776cae9d03706e3d167cf6748aa1e9b9
Brendon O'Laney <brendon.olaney@protonmail.com>
2021-03-06 22:30:21 -0800

    Restructure for templating

--- build.sh
+++ build.sh
@@ -6,39 +6,40 @@ render_file() {
     input_dir="$1"
     input_file="$2"
 
-    output_dir="${input_dir}"
-    mkdir -p "www/${output_dir}"
+    output_dir="www${input_dir#src/site}"
+    mkdir -p "${output_dir}"
 
-    printf "rendering src%s/%s as " "${input_dir}" "${input_file}"
+    printf "rendering %s/%s as " "${input_dir}" "${input_file}"
     case "${input_file}" in
         *.org)
             output_file="${input_file%.org}.html"
-            pandoc --standalone --from org --to html5 \
-                --output="www/${output_dir}/${output_file}" \
-                "src/${input_dir}/${input_file}"
+            pandoc --from org --to html5 \
+                --template="src/template.html" \
+                --output="${output_dir}/${output_file}" \
+                "${input_dir}/${input_file}"
             ;;
         *)
             output_file="${input_file}"
-            cp "src/${input_dir}/${input_file}" "www/${output_dir}/${output_file}"
+            cp "${input_dir}/${input_file}" "${output_dir}/${output_file}"
             ;;
     esac
-    printf "%s\n" "www${output_dir}/${output_file}"
+    printf "%s\n" "${output_dir}/${output_file}"
 }
 
 main () {
-    for file in src/*.*
+    for file in src/site/*.*
     do
         input_file="$(basename "${file}")"
-        render_file "" "${input_file}"
+
+        render_file "src/site" "${input_file}"
     done
 
-    for file in src/**/*.*
+    for file in src/site/**/*.*
     do
-
         input_dir="$(dirname "${file}")"
         input_file="$(basename "${file}")"
 
-        render_file "${input_dir#src}" "${input_file}"
+        render_file "${input_dir}" "${input_file}"
     done
 }
 
